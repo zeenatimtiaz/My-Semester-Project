f5bfedd9061a7302069a8de1f02ee6a22ea34378
zeenat <zeenatimtiaz11@gmail.com>
2026-05-17 00:31:58 +0500

    Added product management system

---
+++ product.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+# Database connection
+def connect():
+    conn = sqlite3.connect("database/olx.db")
+    return conn
+
+
+# Add new product
+def add_product(title, price, description, seller):
+
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO products (title, price, description, seller)
+    VALUES (?, ?, ?, ?)
+    """, (title, price, description, seller))
+
+    conn.commit()
+    conn.close()
+
+
+# Get all products
+def get_products():
+
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM products")
+
+    products = cursor.fetchall()
+
+    conn.close()
+
+    return products
