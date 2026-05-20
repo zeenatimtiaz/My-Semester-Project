9f3092011e9e6b61d48ed32fe04de711f1591cf3
Arsalan <f2024105106@umt.edu.pk>
2026-05-20 10:11:44 +0500

    change in register window

--- Gui.py
+++ Gui.py
@@ -3,6 +3,7 @@ from auth import register_user
 # window and buttons created
 
 from tkinter import *
+from tkinter import messagebox
 window = Tk()
 window.title("OLX Desktop Clone")
 window.geometry("700x500")
@@ -22,26 +23,28 @@ tittle.pack(pady=20)
  
 def open_register() :
     register_window=Toplevel(window)
+    register_window.configure(bg="#ffffff")
     register_window.title("Register")
     register_window.geometry("400x300")
+    heading=Label(register_window,text="Create Your Account",font=("Arial",18,"bold"),bg="white",fg="darkgreen")
 
     #  Name Box
 
-    Label(register_window,text="Name").pack()
-    name_entry=Entry(register_window)
-    name_entry.pack()
+    Label(register_window,text="Name",font=("Arial",12,"bold"),bg="white").pack()
+    name_entry=Entry(register_window,width=30,font=("Arial",11))
+    name_entry.pack(pady=5)
 
     # Email Box
 
-    Label(register_window,text="Email").pack()
-    email_entry=Entry(register_window)
-    email_entry.pack()
+    Label(register_window,text="Email",font=("Arial",12,"bold"),bg="white").pack()
+    email_entry=Entry(register_window,width=30,font=("Arial",11))
+    email_entry.pack(pady=5)
 
     # Password Box
 
-    Label(register_window , text="Password").pack()
-    password_entry=Entry(register_window,show="*")
-    password_entry.pack()
+    Label(register_window , text="Password",font=("Arial",12,"bold"),bg="white").pack()
+    password_entry=Entry(register_window,show="*",width=30,font=("Arial",11))
+    password_entry.pack(pady=5)
 
     # now save user register data 
 
@@ -49,9 +52,12 @@ def open_register() :
         name=name_entry.get()
         email=email_entry.get()
         password=password_entry.get()
+        if name=="" or email=="" or password=="":
+            messagebox.showerror("Error!..Please fill all the fields first")
+            return
 
         register_user(name,email,password)
-        Label(register_window, text="User register Successfully !",fg="green").pack()
+        messagebox.showinfo("Success.!!", "User Registered Successfully")
     Button(register_window,text="Register", command=save_user).pack(pady=10)
 # register button
 register_btn=Button(window, text="Register" , width=20, bg="green" , fg="white" ,font=("Arial", 12, "bold"),command=open_register) # connect register window to main window
